2db0811a31bab7313531aefc604f01d993f7ac3d
Dan Mauer <dmauer@aol.com>
2025-08-04 13:16:48 +0000

    Reverted last commit

--- manifests/create.sh
+++
@@ -1,6 +0,0 @@
-argocd app create globomantics \
---repo https://github.com/damauer/globomantics-gitops.git \
---path manifests \
---dest-namespace default \
---dest-server https://kubernetes.default.svc \
---sync-policy=manual
